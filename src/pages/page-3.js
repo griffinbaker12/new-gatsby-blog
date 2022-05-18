92bae426c5fbc20bfcf19753a7130f17b203cba3
griffinbaker12 <griffin_baker12@icloud.com>
2022-05-18 17:23:17 -0500

    initial commit

---
+++ src/pages/page-3.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>HELLO FROM THE 3RD PAGE</h1>
+  </Layout>
+)
+
+export default ThirdPage
